79c0b30252df9be344f8858829aef618df681fa8
Philippe PEREZ <p.perez@it-spirit.com>
2025-05-27 11:33:41 +0200

    Nettoyage du projet pour plus de clarté

--- mcp_app.py
+++
@@ -1,5 +0,0 @@
-# mcp_app.py
-from mcp.server.fastmcp import FastMCP
-
-# single source-of-truth for your MCP server
-mcp = FastMCP("nova_middleware")
--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\PPZ\NOVA\venv
+set VIRTUAL_ENV=C:\Users\PhilippePEREZ\OneDrive - it-spirit.com\NOVA-SERVER\nova-server\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
